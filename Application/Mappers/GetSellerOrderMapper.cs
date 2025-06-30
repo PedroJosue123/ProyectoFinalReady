85909d4f9ebc43dd31c9d83b954ebfefbb46c6e0
bozanilda <nilda.boza@tecsup.edu.pe>
2025-06-30 03:11:19 -0500

    Clasificacion de mappers

---
+++ Application/Mappers/GetSellerOrderMapper.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Mappers;
+
+public class GetSellerOrderMapper
+{
+    public static GetSellerOrderDomain ToDomain(Pedido entity) => new GetSellerOrderDomain(
+        entity.IdPedidosProductosNavigation.FechaSolicitada,entity.IdPedidosProductosNavigation.IdPagoNavigation.FechaPago,
+        entity.IdCompradorNavigation.Userprofile.Name, entity.IdPedidosProductosNavigation.FechaLlegadaAcordada, entity.IdPedidosProductosNavigation.Producto,
+        entity.IdPedidosProductosNavigation.Cantidad, entity.IdPedidosProductosNavigation.Descripcion, entity.IdPedidosProductosNavigation.DireccionEntrega, entity.IdPedidosProductosNavigation.IdPagoNavigation.Monto,
+        entity.IdPedidosProductos
+     
+    );
+    
+   
+}
