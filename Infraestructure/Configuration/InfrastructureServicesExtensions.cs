44d4fa6822f089218186da6c2a3e4e44383e4768
SpiderWick <albert.vargas@tecsup.edu.pe>
2025-06-29 23:39:44 -0500

    Configuration

---
+++ Infraestructure/Configuration/InfrastructureServicesExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class InfrastructureServicesExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Database Connection
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        // Configurar DbContext y la conexión a MySQL
+        services.AddDbContext<TransactivaDbContext>(options =>
+            options.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString)
+            )
+        );
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IAuthService, AuthService >();
+        services.AddScoped<IPaymentServer, PaymentServer >();
+
+        /*
+        // Services Register
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IFileService, FileService>();
+        services.AddScoped<IUploadFileToAzureStorageService, UploadFileToAzureStorageService>();
+        services.AddScoped<IActivityService, ActivityService>();
+        */
+
+        return services;
+    }
+}
