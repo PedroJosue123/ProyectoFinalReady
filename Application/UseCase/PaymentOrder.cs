4f3c46f5b275e78cbeaccf331d638bd254721c02
PedroJosue123 <pedro.ponce@tecsup.edu.pe>
2025-06-30 03:37:27 -0500

    Coreccion UsesCases Send y Payment

--- Application/UseCase/PaymentOrder.cs
+++ Application/UseCase/PaymentOrder.cs
@@ -1,3 +1,11 @@
+using Application.IUseCase;
+using Application.Mappers;
+using Domain.Dtos;
+using Domain.Entities;
+using Domain.Interface;
+using Infraestructure.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Application.UseCase;
 
 public class PaymentOrder(IUnitOfWork unitOfWork, IPaymentServer paymentServer, IAuthService _authService) : IPaymentOrder
