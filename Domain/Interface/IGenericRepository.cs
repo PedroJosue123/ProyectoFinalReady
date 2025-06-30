3d617a0ffdf5b0dabdf8f8ff6d9bd8bfce519e5f
bozanilda <nilda.boza@tecsup.edu.pe>
2025-06-30 02:51:44 -0500

    Configuracion Interfaces

---
+++ Domain/Interface/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Interface;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task AddAsync(T entity);
+    IQueryable<T> GetAll();    
+    Task<IEnumerable<T>> GetALLAsync();
+    Task<T> GetByIdAsync(int id);
+    Task UpdateAsync(int id, T entity);
+    Task DeleteAsync(int id);
+}
