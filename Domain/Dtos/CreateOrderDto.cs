e018ef1334261863eae546b07b1011200ef67d27
Diego Saul Sanchez Mamani <sanchezmamanidiegosaul@gmail.com>
2025-06-29 23:57:43 -0500

    Primer Dto

---
+++ Domain/Dtos/CreateOrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Dtos;
+
+public class CreateOrderDto
+{
+    public string Product { get; set; }
+    public int Quantity { get; set; }
+    public string Supplier { get; set; }
+    public string Status { get; set; } // Enviado por el comprador
+}
