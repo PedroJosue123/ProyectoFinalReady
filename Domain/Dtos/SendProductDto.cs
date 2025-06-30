fa612695070307fd95593a80a87c25a7a0fa61de
Diego Saul Sanchez Mamani <sanchezmamanidiegosaul@gmail.com>
2025-06-30 00:12:32 -0500

    5to Dtos

---
+++ Domain/Dtos/SendProductDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Dtos;
+
+public class SendProductDto
+{
+
+    
+
+    public string NombreEmpresa { get; set; } = null!;
+
+    public string RucEmpresa { get; set; } = null!;
+
+    public string Asesor { get; set; } = null!;
+
+    public string NumeroTelefonico { get; set; } = null!;
+
+    public string DireccionEnvio { get; set; } = null!;
+
+    public string DireccionRecojo { get; set; } = null!;
+
+    public DateTime FechaLlegada { get; set; }
+
+    public string NroGuia { get; set; } = null!;
+}
