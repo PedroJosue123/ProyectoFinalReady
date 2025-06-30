771bcedc4ef2ecc011d700ab3d51855d97f45917
JosueUrquizo <josueurquizo703@gmail.com>
2025-06-30 00:38:33 -0500

    Actualizacion 2

---
+++ Application/IUseCase/IOrderRequests.cs
@@ -0,0 +1,11 @@
+namespace Application.IUseCase;
+
+public interface IOrderRequests
+{
+
+    Task<List<OrderGetRequestDomain>> GetSolicitud(int id);
+    
+    Task<int> AceptarSolicitud(int id);
+
+    Task<int> VerSiPago(int id);
+}
