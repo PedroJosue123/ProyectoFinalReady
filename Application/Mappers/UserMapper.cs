c7e4b4d404257f3a354282c141bbcaba6075327b
bozanilda <nilda.boza@tecsup.edu.pe>
2025-06-30 03:19:21 -0500

    Mapper User

--- Application/Mappers/UserMapper.cs
+++ Application/Mappers/UserMapper.cs
@@ -2,5 +2,19 @@
 
 public class UserMapper
 {
-    
+    public static UserDomain ToDomain(User entity) => new UserDomain(
+        entity.UserId, entity.Email, entity.Password, entity.CreatedAt,
+        entity.UserTypeId, entity.FailedLoginAttempts ?? 0, entity.LockoutUntil);
+
+    public static User ToEntity(UserDomain domain) => new User
+    {
+        UserId = domain.Id,
+        Email = domain.Email,
+        Password = domain.PasswordHash,
+        CreatedAt = domain.CreatedAt,
+        UserTypeId = domain.UserTypeId,
+        FailedLoginAttempts = domain.FailedLoginAttempts,
+        LockoutUntil = domain.LockoutUntil
+    };
 }
+
